de396ca1fbb876cb75f23907a30681c498855ace
Ahmet Salih <ahmet.salih@windowslive.com>
2016-10-20 17:22:55 +0300

    init

---
+++ long-encode/Lets.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace long_encode
+{
+    public class Lets
+    {
+        private const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        private readonly int Base = Alphabet.Length;
+
+        public string Encode(long i)
+        {
+            if (i == 0) return Alphabet[0].ToString();
+
+            var s = string.Empty;
+
+            while (i > 0)
+            {
+                s += Alphabet[(int)(i % Base)];
+                i = i / Base;
+            }
+
+            return string.Join(string.Empty, s.Reverse());
+        }
+
+        public long Decode(string s)
+        {
+            long i = 0;
+
+            foreach (var c in s)
+            {
+                i = (i * Base) + Alphabet.IndexOf(c);
+            }
+
+            return i;
+        }
+    }
+}
---
+++ long-encode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace long_encode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Lets().Encode(long.MaxValue));
+        }
+    }
+}
